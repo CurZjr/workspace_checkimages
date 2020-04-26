0fb20857ece0d85d427c51175179311d7a5839b8
CurZjr <63982489+CurZjr@users.noreply.github.com>
2020-04-26 09:29:15 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+def calculates_results_stats(results_dic):
+    results_stats_dic = dict()
+    
+    results_stats_dic['n_dogs_img']=0
+    results_stats_dic['n_match']=0
+    results_stats_dic['n_correct_dogs']=0
+    results_stats_dic['n_correct_notdogs']=0
+    results_stats_dic['n_correct_breed']=0     
